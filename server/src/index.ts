6462970503a40977ca4cb076c77cc3a4c5a79a67
MarcinDev7 <proth769@gmail.com>
2022-09-12 20:13:40 +0200

    Init commit

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import * as dotenv from "dotenv";
+dotenv.config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, () => {
+  return console.log(`Server is running`);
+});
