f6ed5a591c7a2aec11b11397108aaadc1f6e8848
MarcinDev7 <proth769@gmail.com>
2022-09-13 10:02:13 +0200

    Added cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,20 @@
 import express from "express";
+import cors from "cors";
 import * as dotenv from "dotenv";
 import router from "./router";
 import { connectToDb } from "./services/db.service";
 
 dotenv.config();
+
 const app = express();
 connectToDb().then(() => {
+  app.use(cors());
+  // app.use((req, res, next) => {
+  //   res.header("Access-Control-Allow-Origin", "*");
+  //   next();
+  // });
   app.use(express.json());
+  // app.use(cors);
   app.use(router);
 
   app.listen(process.env.PORT, () => {
